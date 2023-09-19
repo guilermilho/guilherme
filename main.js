5c880cdd6b74d84267284c5928a4fb962c28e1c3
guilermilho <106349652+guilermilho@users.noreply.github.com>
2023-09-19 07:55:20 -0300

    g

--- main.js
+++ main.js
@@ -11,16 +11,3 @@ while (Contador < 9) {
     Contador = Contador + 1;
     console.log(Contador);
 }
-function TocaSomPom (){
-    document.querySelector('#som_tecla_clap').play();
-}
-
-const ListaDeSons = document.querySelectorAll('.tecla');
-
-let Contador = 0;
-
-while (Contador < 9) {
-    ListaDeSons[0].onclick = TocaSomClap;  
-    Contador = Contador + 1;
-    console.log(Contador);
-}
