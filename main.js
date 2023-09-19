1eb052be82beaee711955edda51502c4d10052ff
guilermilho <106349652+guilermilho@users.noreply.github.com>
2023-09-19 08:44:30 -0300

    yh

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const ListaDeSons = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
+const = lista[contador].classList[1];
+console.log(classe)
+
 while (Contador <ListaDeSons.length) {
     lista[contador].onclick = function(){
         tocar('#som_tecla_toin');
