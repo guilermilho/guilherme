aa722354fd4aecababe2fecc6cabeac0f8f7f51c
guilermilho <106349652+guilermilho@users.noreply.github.com>
2023-09-19 07:51:29 -0300

    m

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-document.querySelector('.tecla_pom');
-document.querySelector('#som_tecla_pom').play;
+function TocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const ListaDeSons = document.querySelectorAll('.tecla');
+
+let Contador = 0;
+
+while (Contador < 9) {
+    ListaDeSons[0].onclick = TocaSomPom;  
+    Contador = Contador + 1;
+    console.log(Contador);
+}
