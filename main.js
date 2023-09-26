2ff80a9015bb36ccf629680e7b2a6fa8b6365c37
guilermilho <106349652+guilermilho@users.noreply.github.com>
2023-09-26 08:04:39 -0300

    vggg

--- main.js
+++ main.js
@@ -7,16 +7,20 @@ const ListaDeSons = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < lista.length) {
-    const classe = Lista[Contador].classList[1];
+    const tecla = lista[contador]
+    const classe = tecla.classList[1];
     //console.log(classe);
 
     const idSom = `#som_${classe}`;
     //console.log(idSom);
 
-    lista[Contador].onclick = function(){
+    tecla.onclick = function(){
         tocar(idSom);
     }
     
     Contador = Contador + 1;
     //console.log(Contador);
+
+tecla.ClassList.add('ativa');
+tecla.ClassLista.remove(ativa);
 }
