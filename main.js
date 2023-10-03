f869529821a2f54178951743d90d0c93dcbcb944
guilermilho <106349652+guilermilho@users.noreply.github.com>
2023-10-03 08:39:36 -0300

    f

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const lista = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
-while (Contador < lista.length) {
+for (Contador < lista.length) {
     const tecla = lista[Contador];
     const classe = tecla.classList[1];
     //console.log(classe);
