ec369a1b652fdcf61cb91e93fc489f453c01807b
guilermilho <106349652+guilermilho@users.noreply.github.com>
2023-10-03 07:51:03 -0300

    j

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ while (Contador < lista.length) {
     tecla.onkeydown = function (){
 tecla.classList.add('ativa');
 console.log(evento.code === 'Space'||evento.code === 'Enter');
-
     }
     tecla.onkeyup = function(){
 tecla.classList.remove('ativa');
